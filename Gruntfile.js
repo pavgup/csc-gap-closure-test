489f14ab48889d9411fbabac8ec10a1d3aeb3696
Ubuntu <ubuntu@ip-10-63-158-155.ec2.internal>
2014-06-23 01:52:09 +0000

    quick pre-demo push

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function (grunt) {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
-        livereload: 8080
+        livereload: 8000
       },
       livereload: {
         options: {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,21 +16,35 @@ angular.module('gapCloseApp')
     ];
   });
 
-function TypeaheadCtrl($scope, $http) {
+function TypeaheadCtrl ($scope, $http) {
   // Any function returning a promise object can be used to load values asynchronously
-  $scope.getLocation = function(val) {
-    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
-      params: {
-        address: val,
-        sensor: false
-      }
-    }).then(function(res){
-      var addresses = [];
+  $scope.getOrgCode = function(val) {
+    return $http.get('http://ec2-54-83-169-13.compute-1.amazonaws.com:5000/v1.0/project_metadata/org_code/'+val)
+    .then(function(res){
+      var select = [];
       angular.forEach(res.data.results, function(item){
-        addresses.push(item.formatted_address);
+        select.push(item[15]);
       });
-      return addresses;
+      //var checkComplete = $http.get('http://ec2-54-83-169-13.compute-1.amazonaws.com:5000/v1.0/project_metadata?pn='+$scope.projectNumber+'&org='+$scope.orgCode)
+      //if(checkComplete.data.results.length === 1) {
+      //  $scope.successVar = "btn btn-lg btn-good";
+      //}
+      return select;
     });
   };
-
-} 
+  $scope.getProjectNumber = function(val) {
+    return $http.get('http://ec2-54-83-169-13.compute-1.amazonaws.com:5000/v1.0/project_metadata/project_number/'+val)
+    .then(function(res){
+      var select = [];
+//      if(res.data.results.length === 1) {
+//      if($http.get('http://ec2-54-83-169-13.compute-1.amazonaws.com:5000/v1.0/project_metadata/project_number_all/'+val).data.results.length === 1) {
+//          $scope.orgCode = 'a';
+//        }
+//      }
+      angular.forEach(res.data.results, function(item){
+        select.push(item[1]);
+      });
+      return select;
+    });
+  };
+}
